490b0ec7483a2bb048124ce4ab491df2f229bc50
whatevercalled <96175413+whatevercalled@users.noreply.github.com>
2024-03-11 11:59:05 +0800

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,37 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject } from '@angular/core';
+import {
+  IsActiveMatchOptions,
+  Router,
+  provideRouter,
+  withViewTransitions,
+} from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { signInterceptor } from './Interceptor/sign.interceptor';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),provideHttpClient(withInterceptors([signInterceptor]))]
+  providers: [
+    provideRouter(
+      routes,
+      withViewTransitions({
+        onViewTransitionCreated: ({ transition }) => {
+          const router = inject(Router);
+          const targetUrl = router.getCurrentNavigation()!.finalUrl!;
+          // Skip the transition if the only thing
+          // changing is the fragment and queryParams
+          const config: IsActiveMatchOptions = {
+            paths: 'exact',
+            matrixParams: 'exact',
+            fragment: 'ignored',
+            queryParams: 'ignored',
+          };
+
+          if (router.isActive(targetUrl, config)) {
+            transition.skipTransition();
+          }
+        },
+      })
+    ),
+    provideHttpClient(),
+  ],
 };
